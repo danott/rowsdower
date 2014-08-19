c8f3704e0cf5dc9a8a2d391292544f73a4bf6466
Dan Ott <thedanielott@gmail.com>
2014-08-18 17:51:18 -0700

    npm run build

---
+++ dist/bookmarklet.js
@@ -0,0 +1,3 @@
+
+(function(){var rowsdowered,rowsdowerize;rowsdowered=false;rowsdowerize=function(){var head,script;if(rowsdowered){return;}
+head=document.getElementsByTagName('head')[0];script=document.createElement('script');script.src='https://raw.github.com/danott/rowsdower/master/min/rowsdower.js';head.appendChild(script);return rowsdowered=true;};rowsdowerize();}).call(this);
--- min/bookmarklet.js
+++
@@ -1,1 +0,0 @@
-var rowsdowered,rowsdowerize;rowsdowered=!1;rowsdowerize=function(){var a,b;if(rowsdowered)return;a=document.getElementsByTagName("head")[0];b=document.createElement("script");b.src="https://raw.github.com/danott/rowsdower/master/min/rowsdower.js";a.appendChild(b);return rowsdowered=!0};rowsdowerize();
